8d934e5144be16e304e363e3286d5981742819f3
Vilas-Rathod <vilasrj958@gmail.com>
2025-02-14 16:06:53 +0530

    "CorsConfig added"

--- src/main/java/com/vilas/product_todo_api/controller/CorsConfig.java
+++ src/main/java/com/vilas/product_todo_api/controller/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Change if needed
+                        .allowedOrigins("http://localhost:5173") // Change if needed
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
